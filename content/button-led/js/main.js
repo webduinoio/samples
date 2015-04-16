71bd6a39024de631183d11c9100961afa7d91727
Ke, Mingze <mingze.ke@gmail.com>
2015-04-17 05:55:20 +0800

    Add button+led

---
+++ content/button-led/js/main.js
@@ -0,0 +1,36 @@
+window.addEventListener('WebComponentsReady', function () {
+  var setBtn = document.getElementById('set'),
+    device = document.getElementById('device'),
+    board = document.getElementById('board');
+
+  msg.className = "ready";
+  device.setAttribute('value', localStorage.device || "");
+
+  setBtn.addEventListener('click', function (e) {
+    msg.className = "setting";
+
+    board.device = device.value;
+    board.on('ready', ready);
+    board.init();
+
+    localStorage.device = device.value;
+
+    e.stopPropagation();
+    e.preventDefault();
+    return false;
+  }, false);
+
+  function ready() {
+    var led = document.getElementById('led'),
+      button = document.getElementById('button'),
+      light = document.getElementById('light');
+
+    button.on('pressed', function () {
+      led.on();
+      light.className = 'on';
+    });
+
+    msg.className = "set";
+  };
+
+}, false);
