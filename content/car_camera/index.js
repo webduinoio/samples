d880416dad8a0f6f0ef87dc08232eaad843c43e1
Marty <marty@kingkit101.com>
2019-03-18 13:43:03 +0800

    fix servo start pos

--- content/car_camera/index.js
+++ content/car_camera/index.js
@@ -1,11 +1,9 @@
-
-
 var param = location.hash.substring(1).split('@');
 
-console.log("deviceID:",param[0]);
-console.log("camera URL:",param[1]);
+console.log("deviceID:", param[0]);
+console.log("camera URL:", param[1]);
 
-new Camera(param[1] /*"http://192.168.0.251:81/stream"*/).onCanvas('c1',function(c){
+new Camera(param[1] /*"http://192.168.0.251:81/stream"*/ ).onCanvas('c1', function (c) {
 
 });
 
@@ -37,41 +35,47 @@ function controllerBtnEvent(c, e, callback) {
 }
 
 
-boardReady({board: 'Smart', device: param[0], transport: 'mqtt'}, function (board) {
+boardReady({ board: 'Smart', device: param[0], transport: 'mqtt' }, function (board) {
   document.querySelector("#demo-area-09 .btn-show").innerHTML = 'Ready !';
   board.samplingInterval = 50;
+  setTimeout(function () {
+    console.log("set servo");
+    servo.angle = 170;
+    document.querySelector("#demo-area-09 .btn-show").innerHTML = '170';
+  }, 1000);
+
   servo = getServo(board, 0);
   car = getToyCar(board, 14, 16, 2, 5);
-  car.setLeftSpeed(77);
-  controllerBtnEvent(getElement('#demo-area-09 .btn-up'),['mousedown', 'touchstart'], function () {
+  //car.setLeftSpeed(77);
+  controllerBtnEvent(getElement('#demo-area-09 .btn-up'), ['mousedown', 'touchstart'], function () {
     car.goFront();
   });
-  controllerBtnEvent(getElement('#demo-area-09 .btn-down'),['mousedown', 'touchstart'], function () {
+  controllerBtnEvent(getElement('#demo-area-09 .btn-down'), ['mousedown', 'touchstart'], function () {
     car.goBack();
   });
-  controllerBtnEvent(getElement('#demo-area-09 .btn-left'),['mousedown', 'touchstart'], function () {
+  controllerBtnEvent(getElement('#demo-area-09 .btn-left'), ['mousedown', 'touchstart'], function () {
     car.turnLeft();
   });
-  controllerBtnEvent(getElement('#demo-area-09 .btn-right'),['mousedown', 'touchstart'], function () {
+  controllerBtnEvent(getElement('#demo-area-09 .btn-right'), ['mousedown', 'touchstart'], function () {
     car.turnRight();
   });
-  controllerBtnEvent(getElement('#demo-area-09 .btn-up'),['mouseup', 'touchend'], function () {
+  controllerBtnEvent(getElement('#demo-area-09 .btn-up'), ['mouseup', 'touchend'], function () {
     car.stop();
   });
-  controllerBtnEvent(getElement('#demo-area-09 .btn-down'),['mouseup', 'touchend'], function () {
+  controllerBtnEvent(getElement('#demo-area-09 .btn-down'), ['mouseup', 'touchend'], function () {
     car.stop();
   });
-  controllerBtnEvent(getElement('#demo-area-09 .btn-left'),['mouseup', 'touchend'], function () {
+  controllerBtnEvent(getElement('#demo-area-09 .btn-left'), ['mouseup', 'touchend'], function () {
     car.stop();
   });
-  controllerBtnEvent(getElement('#demo-area-09 .btn-right'),['mouseup', 'touchend'], function () {
+  controllerBtnEvent(getElement('#demo-area-09 .btn-right'), ['mouseup', 'touchend'], function () {
     car.stop();
   });
   var p;
   var range = document.querySelector('.demo-area-09-input');
   range.setAttribute('min', 130);
   range.setAttribute('max', 170);
-  range.setAttribute('step', 5);
+  range.setAttribute('step', 1);
   range.setAttribute('value', 180);
   p = Math.round((180 - 130) * 100 / (180 - 130));
   range.style.backgroundImage = '-webkit-linear-gradient(left, #246 0%, #246 ' + p + '%, #222 ' + p + '%, #222 100%)';
