9131ba8f57684bb475e5190d5dc155bccbfa3d0f
oxxo <oxxo.studio@gmail.com>
2020-09-10 09:27:39 +0800

    edit js

--- content/kebbi-demo-202009/js/test.js
+++ content/kebbi-demo-202009/js/test.js
@@ -1,2 +1,16 @@
-console.log(window.Nuwa);
-window.Nuwa.say('測試一下');
+!async function () {
+  console.log(window.Nuwa);
+  //window.Nuwa.say('測試一下');
+
+  function lottery() {
+    let result = [];
+    for (let i = 0; i < 6; i++) {
+      let num = ~~(Math.random() * 49);
+      if (!result.includes(num)) {
+        result.push(num);
+      }
+    }
+    return result.sort((x, y) => x - y);
+  }
+  window.Nuwa.say(lottery());
+}();
